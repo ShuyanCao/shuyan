8a440819c566599e50a2ecea327542299fe06b28
yayu <13717626@student.uts.edu.au>
2020-06-09 17:10:09 +1000

    //project

---
+++ assignment3-teamHotpot/assignment3-teamHotpot/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  assignment3-teamHotpot
+//
+//  Created by 罗雅愉 on 9/6/2020.
+//  Copyright © 2020 罗雅愉. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
